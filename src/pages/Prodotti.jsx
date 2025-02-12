5971cb830d2bc013d27a21643ea22b5ac4ad2a0a
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-12 02:54:15 +0100

    feat: add products with axios get method

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,3 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Prodotti() {
-  return <></>;
+  const [products, setProducts] = useState([]);
+
+  function fetchProducts() {
+    axios.get("http://localhost:3000/foods").then((res) => {
+      setProducts(res.data);
+    });
+  }
+
+  useEffect(fetchProducts, []);
+
+  return (
+    <>
+      <h1>I nostri prodotti:</h1>
+      {products.map((product) => {
+        return (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>{product.content}</p>
+          </li>
+        );
+      })}
+    </>
+  );
 }
