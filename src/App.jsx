867c07dc65c9b5401ca21753dd059734944673bf
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-12 02:01:33 +0100

    feat: add pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //Pages
 import Homepage from "./pages/Homepage";
+import AboutUs from "./pages/AboutUs";
+import Contatti from "./pages/Contatti";
+import Prodotti from "./pages/Prodotti";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={Homepage} />
+        <Route path="/about-us" Component={AboutUs} />
+        <Route path="/contatti" Component={Contatti} />
+        <Route path="/Prodotti" Component={Prodotti} />
+        <Route path="*" Component={NotFoundPage} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,3 @@
+export default function NotFoundPage() {
+  return <h1>ERRORE 404 - La pagina non è stata trovata</h1>;
+}
